19139fe092b2ce55d84cd650a7dcd4cb5c0b27e5
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-21 15:20:07 +0100

    start

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// Esercizio:
+// Riscrivere questa pagina del sito google
+// https://policies.google.com/faq.
+// Ci sono diverse domande con relative risposte.
+// Gestire il “Database” e la visualizzazione di
+// queste domande e risposte con PHP.
+
+
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Google-faq</title>
+  </head>
+  <body>
+
+
+  </body>
+</html>
+
+<!-- ESEMPI -->
+<!-- <h1>Nome completo: <?php echo $nome . " " . $cognome; ?></h1>  STAMPA CON CONCATENAZIONE TRAMITE "."
+<h2>Nome: <?php echo $nome; ?></h2>       STAMPA SOLO NOME
+<h1>nome:  <?php echo $nome . ", " . "numero lettere:" . strlen($nome); ?>  </h1> STRLEN: NUMERO DI LETTERE DI CUI è COMPOSTO IL "NOME"
+<h1>nome:  <?php  strpos($nome, "a") ?> VERIFICA NEL "NOME", IN CHE POSIZIONE SI TROVA LA LETTERA "A"
+<?php is_numeric($nome) ?>    VERIFICA CHE L'ELEMENTO SIA UN NUMERO
+<h2>Cognome: <?php echo $cognome; ?></h2>  STAMPA SOLO COGNOME
+<p>Variabile GET: <?php echo $_GET["nome"]; ?></p>  STAMPA CON RICHIAMO A VARIABILE
+<p><?php var_dump($arrayText); ?></p>   STAMPA ARRAY
+<h4><?php echo trim($arrayText[1]); ?></h4>  STAMPA SECONDO ELEMENTO IN ARRAY ( 0, "1", 2...)
+<?php echo str_replace("dolor", "***", $text); ?>  SOSTITUISCI IN "TEXT" LA PRIMA PAROLA(DOLOR) CON LA SECONDA (***)
+<h5><?php echo ucwords($text); ?></h5> -->
+<!-- __________________CICLI_____________________________ -->
+  <!-- <?php for($i = 0; $i < count($array); $i++)  ?>  CICLO FOR, COUNT = .LENGHT
+  <?php foreach($matches as $match){ echo $match . "<br>"}?>   UGUALE COME UN CICLO FOR, NELL'ARRAY MATCHES PARTE IL CICLO E STAMPA TUTTI I MATCH
+  <?php foreach ($matches as $key => $value): ?> QUALORA ALL'INTERNO DELL'ARRAY CI SIANO PIù CONDIZIONI : $KEY(STUDENTE) => $VALUE(LEONARDO)
+
+  <?php endforeach; ?>-->
+<!-- _________________________________________________________ -->
+  <!-- localhost:8888/php-snacks-blocco-1/index2.php?... PER APRIRE NUOVO INDEX -->
+<!-- /ESEMPI -->

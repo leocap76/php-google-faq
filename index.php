1584a65498baf25799f3012f9eb3fb76d461d5c0
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-22 10:22:13 +0100

    prova di collegamento

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 $googleFaq = [
 
   [
-    "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+    "question" => "Come state implementando la recente decisione della Corte di giustizia dell'<a href='https://www.dafont.com/it/'>Unione europea</a> (CGUE) relativa al diritto all'oblio?",
     "answer" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
 
 Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
@@ -96,6 +96,7 @@ Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue info
         <?php echo $value["answer"] . "<br>"; ?>
       </p>
     <?php } ?>
+
     </div>
   </body>
 </html>
